ce914a8f67d7517b8650f8f76d6335c37ce86651
Raphael Quaye <rquaye023@st.ug.edu.gh>
2024-08-18 17:50:43 +0100

    Overview

---
+++ frontend/src/components/Overview.js
@@ -0,0 +1,25 @@
+// src/components/Home.js
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import './style/Overview.css';
+
+const Overview = () => {
+  const navigate = useNavigate();
+
+  return (
+    <div className="overview-container">
+    <h1>Welcome To KOBOWISE</h1>
+    <button onClick={() => navigate('/adminlogin')}>Admin</button>
+    <button onClick={() => navigate('/cashierlogin')}>Cashier</button>
+      <main className="overview-content">
+      </main>
+
+        <footer>
+            <p>© 2024 POS System. All rights reserved.</p>
+        </footer>
+
+    </div>
+  );
+};
+
+export default Overview;
