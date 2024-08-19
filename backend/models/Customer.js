d50434baab1d1a956fda4cd70b96a75b49c9a3f5
Raphael Quaye <rquaye023@st.ug.edu.gh>
2024-08-19 02:47:25 +0100

    Customer model

---
+++ backend/models/Customer.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const CustomerSchema = new mongoose.Schema({
+  id: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  phone: {
+    type: String
+  }
+});
+
+module.exports = mongoose.model('Customer', CustomerSchema);
