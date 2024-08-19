3215aff584551492d1eb000b170ac6162c040c4d
Raphael Quaye <rquaye023@st.ug.edu.gh>
2024-08-19 02:49:15 +0100

    User model

---
+++ backend/models/User.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const UserSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  role: {
+    type: String,
+    required: true,
+    enum: ['admin', 'cashier']
+  }
+});
+
+// Hash the password before saving
+UserSchema.pre('save', async function (next) {
+  if (this.isModified('password') || this.isNew) {
+    this.password = await bcrypt.hash(this.password, 10);
+  }
+  next();
+});
+
+// Method to compare passwords
+UserSchema.methods.comparePassword = async function (candidatePassword) {
+  return bcrypt.compare(candidatePassword, this.password);
+};
+
+module.exports = mongoose.model('User', UserSchema);
